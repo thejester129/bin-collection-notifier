67eecae8dd7f5502845aa75e3944a1f8af9083c7
Andrew Hollis <thejester129@gmail.com>
2022-11-07 23:43:28 +0000

    init

---
+++ backend-notifier/index.js
@@ -0,0 +1,57 @@
+const fetch = require("node-fetch");
+const { Expo } = require("expo-server-sdk");
+
+exports.handler = async (event) => {
+  const EXPO_PUSH_TOKEN = process.env.EXPO_PUSH_TOKEN;
+  const BASE_URL = "https://www.falkirk.gov.uk/bin-calendar";
+  const CUSTOM_ID = "uprn=136065096";
+
+  var today = new Date();
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const tomorrowFormatted = today.toISOString().split("T")[0];
+  const url = `${BASE_URL}?${CUSTOM_ID}&start=${tomorrowFormatted}&end=${tomorrowFormatted}`;
+
+  console.log("Sending request for " + tomorrowFormatted);
+
+  const response = await fetch(url);
+  const results = await response.json();
+
+  console.log("Results: " + JSON.stringify(results));
+
+  // Time to put out some bins
+  const binsToPutOut =
+    results &&
+    results
+      .map((x) => x.title)
+      .reduce((prev, current) => {
+        if (prev) {
+          return prev + ", " + current;
+        } else {
+          return current;
+        }
+      }, "");
+  let expo = new Expo();
+  // Check that all your push tokens appear to be valid Expo push tokens
+  if (!Expo.isExpoPushToken(EXPO_PUSH_TOKEN)) {
+    console.error(
+      `Push token ${EXPO_PUSH_TOKEN} is not a valid Expo push token`
+    );
+    return;
+  }
+
+  // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+  const message = {
+    to: EXPO_PUSH_TOKEN,
+    sound: "default",
+    title: "HAW",
+    body:
+      results && results.length
+        ? `Better put out the ${binsToPutOut} for tomorrow!`
+        : "No bins to put out. Enjoy staying inside :)",
+    data: { color: "ffffff" },
+  };
+
+  console.log("Sending expo notification");
+  await expo.sendPushNotificationsAsync([message]);
+};
---
+++ frontend-app/App.tsx
@@ -0,0 +1,88 @@
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<any>(false);
+  const notificationListener = useRef<any>();
+  const responseListener = useRef<any>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>It's bin a while..</Text>
+    </View>
+  );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
